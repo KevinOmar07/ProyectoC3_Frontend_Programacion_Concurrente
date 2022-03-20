32f9940831d12175b9ecce8f5c6aba12a289a359
Karol05 <201219@ids.upchiapas.edu.mx>
2022-03-19 18:07:43 -0600

    Sign up

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -3,15 +3,63 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 class SignUp extends React.Component{
-    constructor(props) {
-        super(props);
+    constructor() {
+        super()
+        this.state = {
+
+        }
     };
+
+    changeField(e) {
+        let field = e.target.name
+        let value = e.target.value
+
+        this.setState(update(this.state,{
+            [field] : {$set : value}
+        }))
+    }
+
+    registrar(){
+        this.messageError.innerHTML = ''
+        this.validarCampos()
+        if (this.status && this.usernameOk) {
+            let user = {
+                username: this.state.username,
+                password: this.state.password,
+                confpassword: this.state.confpassword
+            }
+        }
+    }
+
+    validarCampos(){
+
+
+    }
 
     render() {
         return(
-            <>
-                <h1>Aqui ira el registro</h1>
-            </>
+          <React.Fragment>
+              <div className="container position-absolute top-50 start-50">
+                  <h1> REGISTRARSE</h1>
+                  <form onSubmit={this.registrar.bind(this)}>
+                    <div>
+                        <label htmlFor="user"> Nombre de usuario</label>
+                        <br/>
+                        <input type="text" name="user" id="user" placeholder="Nombre de usuario"/>
+                        <br/>
+                        <label htmlFor="password"> Contraseña</label>
+                        <br/>
+                        <input type="text" name="password" id="password" placeholder="Contraseña"/>
+                        <br/>
+                        <label htmlFor="confirmpass"> Confirmar contraseña </label>
+                        <br/>
+                        <input type="text" name="confirmpass" id="confirmpass" placeholder="Confirmar contraseña"/>
+                        <br/>
+                        <input type="submit" value="Enviar"/>
+                    </div>
+                  </form>
+              </div>
+          </React.Fragment>
         );
     }
 }
